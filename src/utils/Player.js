a21972ece6d5a2b636c21f97950b5d8fae16012c
DerekMonster <yuyao110120user@gmail.com>
2025-06-23 16:24:37 +0800

    Remove daily check-in

--- src/utils/Player.js
+++ src/utils/Player.js
@@ -309,11 +309,12 @@ export default class {
     );
     const trackDuration = ~~(track.dt / 1000);
     time = completed ? trackDuration : ~~time;
-    scrobble({
-      id: track.id,
-      sourceid: this.playlistSource.id,
-      time,
-    });
+    // Remove daily check-in
+    // scrobble({
+    //   id: track.id,
+    //   sourceid: this.playlistSource.id,
+    //   time,
+    // });
     if (
       store.state.lastfm.key !== undefined &&
       (time >= trackDuration / 2 || time >= 240)
